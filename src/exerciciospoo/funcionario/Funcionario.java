e6a98bb0488cae7112b61f2de962637d63356e5a
Saulo Suassuna de O Laranjeira <saulo.laranjeira@dominos.com.br>
2025-09-09 12:10:35 -0300

    Envio de exercício

--- src/exerciciospoo/funcionario/Funcionario.java
+++ src/exerciciospoo/funcionario/Funcionario.java
@@ -2,4 +2,23 @@ package exerciciospoo.funcionario;
 
 public class Funcionario {
 
+	public String name;
+	public double grossSalary,tax;
+	
+	public double NetSalary() {
+		return grossSalary - tax;
+	}
+	
+	public String toString() {
+		return "Funcionário: "
+		+ name
+		+", "
+		+"$ "
+		+ String.format("%.2f", NetSalary());
+		}
+	
+	public void IncreaseSalary(double percentage) {
+		double increase = grossSalary * (percentage/100);
+		grossSalary +=increase;
+	}
 }
--- src/exerciciospoo/funcionario/Main.java
+++ src/exerciciospoo/funcionario/Main.java
@@ -1,10 +1,34 @@
 package exerciciospoo.funcionario;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Funcionario funcionario = new Funcionario();
+		
+		System.out.println("Name: ");
+		funcionario.name = sc.nextLine();
+		
+		System.out.println("Gross salary: ");
+		funcionario.grossSalary = sc.nextDouble();
+		
+		System.out.println("Tax: ");
+		funcionario.tax = sc.nextDouble();
+		
+		System.out.println(funcionario);
+		
+		System.out.println("Qual a porcentagem do aumento do salário?");
+		double percentage = sc.nextDouble();
+		funcionario.IncreaseSalary(percentage);
+		
+		System.out.println("\nDados atualizados: " + funcionario);
+		
+		sc.close();
 	}
 
 }
